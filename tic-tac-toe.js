e603aae84a7ee034a575ef4e09e5165c9c4bddb5
graheed <graheed@gmail.com>
2023-10-21 20:59:57 -0500

    Finished implementing the functionality that checks if a player wins along with winner announcement text

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,24 @@ window.onload = function() {
     const divElements = document.querySelectorAll("#board div");
     let currentPlayer = "X";
     const gameState = new Array(9).fill(null);
-
+  
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  
+    function checkWinner(player) {
+      return winningCombinations.some(combination =>
+        combination.every(index => gameState[index] === player)
+      );
+    }
+  
     divElements.forEach((element, index) => {
       element.classList.add("square");
   
@@ -12,17 +29,23 @@ window.onload = function() {
           this.classList.add(currentPlayer);
           gameState[index] = currentPlayer;
   
+          if (checkWinner(currentPlayer)) {
+            const divStatus = document.querySelector("#status");
+            divStatus.classList.add("you-won");
+            divStatus.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+          }
+  
           currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
      });
-      element.addEventListener('mouseover', function() {
+  
+     element.addEventListener('mouseover', function() {
         element.classList.add("hover");
-    });
-
-    element.addEventListener('mouseout', function() {
+     });
+  
+     element.addEventListener('mouseout', function() {
         element.classList.remove("hover");
-      });
-
-    });
+     });
+   });
   };
   
